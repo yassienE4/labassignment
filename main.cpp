6aa10e1314a2f49f85f94a5c0f69517767b9388a
yassienE4 <yassienebrahim@gmail.com>
2024-11-07 13:09:08 +0200

    declared class and private variables

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#include <queue>
 using namespace std;
+
+class patient
+{
+    private:
+        int id;
+        char gender;
+        string time;
+        bool urgent;
+    public:
+    
+};
 
 int main()
 {
