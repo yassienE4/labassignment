ec4c006cd08c05a539fda7181d5c56159a62995f
yassienE4 <yassienebrahim@gmail.com>
2024-11-07 13:41:55 +0200

    .

--- main.cpp
+++ main.cpp
@@ -1,15 +1,47 @@
 #include <iostream>
 #include <queue>
+#include <vector>
+#include <time.h>
 using namespace std;
 
-class patient
+
+
+class allpatients
 {
     private:
-        int id;
-        char gender;
-        string time;
-        bool urgent;
-    public:
+        struct patient
+        {
+            int id;
+            char gender;
+            string time;
+            bool urgent;
+        };
+        void gotourgent();
+        void gotonormal();
+        int index;
+        vector<patient> allpatient;
+        queue<patient> urgent;
+        queue<patient> normal;
+    public: 
+        allpatients()
+        {
+            patient temp;
+            for(int i = 0; i< 100; i++)
+            {
+                srand(time(NULL));
+                temp.id = rand()%10000
+                temp.gender = 
+
+            }
+            
+        }
+        void gotoqueue(vector<patient>)
+        {
+
+        }
+        
+
+
     
 };
 
